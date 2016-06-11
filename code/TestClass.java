9a72cff0302e507492540c0b4e4ebc252ecab48d
unknown <>
2016-06-11 10:10:25 +0800

    add some summary files

---
+++ code/TestClass.java
@@ -0,0 +1,11 @@
+public class TestClass {
+
+    private void testMethod() {
+        System.out.println("Hello World!");
+    }
+
+    public static void main(String args[]) {
+        TestClass testClass = new TestClass();
+        testClass.testMethod();
+    }
+}
